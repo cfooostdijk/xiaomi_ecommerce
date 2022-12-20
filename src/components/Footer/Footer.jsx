b7eae4151bf0190f66a67ba254d25077234db1be
CrisOos <cristian.oostdijk@avenida.com>
2022-12-19 21:19:06 -0300

    meh

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,36 @@
-import React from "react";
-
 import IconButton from '@mui/material/IconButton';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
+
+import React from "react";
+import "./Footer.css";
 
 export const Footer = () => {
   return (
-    <Paper sx={{ width: '100%', position: 'fixed', bottom: 0 }} 
-			component="footer" square variant="outlined">
-      <Container maxWidth="lg">
-        <Box sx={{ flexGrow: 1, justifyContent: "center", display: "flex", my:1 }}>
-					<IconButton aria-label="Linkedin.com" onClick={() => window.open('https://www.linkedin.com/in/cfoostdijk/')}>
-  					<LinkedInIcon fontSize="large" />
-					</IconButton>
-					<IconButton aria-label="Linkedin.com" onClick={() => window.open('https://github.com/cfooostdijk')}>
-  					<GitHubIcon fontSize="large" />
-					</IconButton>
-				</Box>
-				<Box sx={{ flexGrow: 1, justifyContent: "center", display: "flex", mb: 2, }}>
-					<Typography variant="caption" color="initial"> Copyright © 2022 All Rights Reserved by Cristian Oostdijk </Typography>
-				</Box>
-			</Container>
-		</Paper>
-	)
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>ANOTHER SHOP STORE</h4>
+						  <Box sx={{ flexGrow: 1, justifyContent: "center", display: "flex", my:1 }}>
+ 								<IconButton aria-label="Linkedin.com" onClick={() => window.open('https://www.linkedin.com/in/cfoostdijk/')}>
+   								<LinkedInIcon fontSize="large" />
+ 								</IconButton>
+ 								<IconButton aria-label="Linkedin.com" onClick={() => window.open('https://github.com/cfooostdijk')}>
+   								<GitHubIcon fontSize="large" />
+ 								</IconButton>
+ 							</Box>
+          </div>
+        </div>
+        <div className="row">
+          <p className="col-sm">
+						Diseñado y desarrollado por Cristian Oostdijk | © 2022 CoderHouse React Course. Todos los derechos reservados.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import styles from './styles/Navbar.module.scss';
 import Brandlogo from '../../assets/Icons/Brandlogo';
 import CartWidget from '../../containers/CartWidget';
 
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -43,56 +44,56 @@ const NavBar = ({categories}) => {
 
   return (
     <div className={styles.navbar}>
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Brandlogo />
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Brandlogo />
 
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-              <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true"
-                onClick={handleOpenNavMenu} color="inherit" >
-                <MenuIcon />
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true"
+              onClick={handleOpenNavMenu} color="inherit" >
+              <MenuIcon />
+            </IconButton>
+            <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+              keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left' }} open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' } }}>
+              {categories.map((category) => { 
+                return ( <MenuItem key={category.id} to={category.route} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{category.name}</Typography>
+                </MenuItem>)})}
+            </Menu>
+          </Box>
+
+          <Box sx={{ flexGrow: 1, color: 'black', display: { xs: 'none', md: 'flex' } }}> {
+            categories.map((category) => {
+              return ( <NavLink key={category.id} to={category.route} onClick={handleCloseNavMenu}>
+                {category.name} </NavLink> )})}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            {/* <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
-              <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-                keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left' }} open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' } }}>
-                {categories.map((category) => { 
-                  return ( <MenuItem key={category.id} to={category.route} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{category.name}</Typography>
-                  </MenuItem>)})}
-              </Menu>
-            </Box>
+            </Tooltip>
+            <Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: 'top',
+                horizontal: 'right' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+              open={Boolean(anchorElUser)} onClose={handleCloseUserMenu}>
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu> */}
+          </Box>
 
-            <Box sx={{ flexGrow: 1, color: 'black', display: { xs: 'none', md: 'flex' } }}> {
-              categories.map((category) => {
-                return ( <NavLink key={category.id} to={category.route} onClick={handleCloseNavMenu}>
-                  {category.name} </NavLink> )})}
-            </Box>
-
-            <Box sx={{ flexGrow: 0 }}>
-              {/* <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: 'top',
-                  horizontal: 'right' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-                open={Boolean(anchorElUser)} onClose={handleCloseUserMenu}>
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu> */}
-            </Box>
-
-            <Box sx={{ flexGrow: 0 }}>
-              <CartWidget />
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </div>
+          <Box sx={{ flexGrow: 0 }}>
+            <CartWidget />
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  </div>
   );
 }
 
