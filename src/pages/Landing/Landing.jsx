719f02259e4e4bb883f18b622a76357d782544f6
CrisOos <cristian.oostdijk@avenida.com>
2022-12-29 16:05:33 -0300

    Entrega Final

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,11 +5,11 @@ import HeroSlide from '../../components/HeroSlide/HeroSlide';
 export const Landing =() =>{
 
   const images = [
-    { image: "http://localhost:3000/image-1.jpg" },
-    { image: "http://localhost:3000/image-2.jpg" },
-    { image: "http://localhost:3000/image-3.jpg" },
-    { image: "http://localhost:3000/image-4.jpg" },
-    { image: "http://localhost:3000/image-5.jpg" },
+    { image: "http://gadgetsmobile.pk/web/image/9153/xiomi_banner.jpg?access_token=7078234b-cf3d-4b5d-a7e3-41b1f4605247" },
+    { image: "https://www.mygsm.me/media/catalog/product/7/_/7_1_12.jpg" },
+    { image: "https://www.inmovil.es/img/cms/Agregar%20un%20ti%CC%81tulo.png" },
+    { image: "https://www.inmovil.es/img/cms/redmi%209%20.png" },
+    { image: "https://www.inmovil.es/img/cms/note%209%20pro.png" },
   ]
 
   const heroStyles = {
