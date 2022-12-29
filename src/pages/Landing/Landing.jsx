bdbe44a87568e1b4d31feecf2dca758ac92a3d1b
CrisOos <cristian.oostdijk@avenida.com>
2022-12-29 16:13:44 -0300

    Entrega Final

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,8 +5,8 @@ import HeroSlide from '../../components/HeroSlide/HeroSlide';
 export const Landing =() =>{
 
   const images = [
-    { image: "http://gadgetsmobile.pk/web/image/9153/xiomi_banner.jpg?access_token=7078234b-cf3d-4b5d-a7e3-41b1f4605247" },
-    { image: "https://www.mygsm.me/media/catalog/product/7/_/7_1_12.jpg" },
+    { image: "https://tecstore.pe/media/bannercito_xiaomi.png" },
+    { image: "https://www.aventurasnerd.com/wp-content/uploads/2021/05/redmi-note-10pro-null-banner.jpg" },
     { image: "https://www.inmovil.es/img/cms/Agregar%20un%20ti%CC%81tulo.png" },
     { image: "https://www.inmovil.es/img/cms/redmi%209%20.png" },
     { image: "https://www.inmovil.es/img/cms/note%209%20pro.png" },
