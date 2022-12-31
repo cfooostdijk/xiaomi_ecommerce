ddf552a15f0b6e7709b6d3fb8794e2d8af1e0a5d
CrisOos <cristian.oostdijk@avenida.com>
2022-12-30 21:01:45 -0300

    Entrega Final

--- src/components/OrderDetail/OrderDetail.jsx
+++ src/components/OrderDetail/OrderDetail.jsx
@@ -7,32 +7,39 @@ const OrderDetail = ({ orderDetail }) => {
 		<div className="receipt">
 			<h2 className="name"> Xiaomi Ecommerce </h2>
 			<p className="greeting"> Thank you for your order! </p>
-
-			{/* <p className="greeting"> {orderDetail.buyer.lastname} </p> */}
-			{/* <p className="greeting"> {orderDetail.buyer.phone} </p> */}
-			
+			<h3 className="greeting"> Name: {orderDetail.buyer.lastname}  {orderDetail.buyer.firstname} </h3>
+			<h3 className="greeting"> Phone: {orderDetail.buyer.phone} </h3>
 			<div className="order">
 				<p> Order No : {orderDetail.orderId} </p>
 				<p> Date : {orderDetail.date} </p>
 			</div>
 			<hr/>
-
-			<div className="details">
+			<div>
 				<h3> Details </h3>
 				<div className="product">
 					<div className="info">
-						{/* <h4> {orderDetail.items[0].title} </h4> */}
-						{/* <p> {orderDetail.items[0].quantity} </p> */}
+						<h4> Product: {orderDetail.items[0].title} <span> $ {orderDetail.items[0].price} </span></h4>
+						<p> Quantity: {orderDetail.items[0].quantity} </p>
 					</div>
 				</div>
-				{/* <p> $ {orderDetail.items[0].price} </p> */}
 			</div>
 
-			<div className="totalprice">
-				<p className="tot"> Total <span> {orderDetail.total} </span> </p>
+			<div>
+				<h3> Total: $ <span> {orderDetail.total} </span> </h3>
 			</div>
 		</div>
 	)
 };
 
 export default OrderDetail;
+
+{/* <div className='king'>
+	<div className='card'>
+		<div className='description'>
+			<h1>{orderDetail.items[0].title}</h1>
+			<h2>$ {orderDetail.items[0].price}</h2>
+			<h5>Quantity: {orderDetail.items[0].quantity}</h5>
+			<h5>Total: $ <span> {orderDetail.total} </h5>
+		</div>
+	</div>
+</div> */}
--- src/containers/OrderContainer/OrderContainer.jsx
+++ src/containers/OrderContainer/OrderContainer.jsx
@@ -2,22 +2,22 @@ import React, { useEffect, useState } from "react";
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import OrderDetail from "../../components/OrderDetail/OrderDetail";
 
-// import ErrorPage from "../../pages/ErrorPage/ErrorPage";
-// import Spinner from "../../assets/Spinners/Spinner";
-// import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import ErrorPage from "../../pages/ErrorPage/ErrorPage";
+import Spinner from "../../assets/Spinners/Spinner";
+
 
 const Order = ({ orderId }) => {
 
 	const [orderDetail, setOrderDetail] = useState([])
-  // const [valid, setValid] = useState(true);
-  // const [loading, setLoading] = useState(false);
+  const [valid, setValid] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 1500);
-  // }, []);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
 console.log(orderId)
 
 useEffect(() => {
@@ -28,21 +28,15 @@ useEffect(() => {
 		.then(order => { setOrderDetail({ orderId: order.id, ...order.data() } )})
 	}, [orderId])
 
-	return (
-		<OrderDetail orderDetail={orderDetail} />
-	)
-}
-
-
-//   if (valid === false) {
-//     return (
-//       <> { loading ? <Spinner /> : <ErrorPage /> } </>
-//     )
-//   } else {
-//     return (
-//       <> { loading ? <Spinner /> : <OrderDetail order={order} /> } </>
-//     )
-//   }
-// };
+  if (valid === false) {
+    return (
+      <> { loading ? <Spinner /> : <ErrorPage /> } </>
+    )
+  } else {
+    return (
+      <> { loading ? <Spinner /> : <OrderDetail orderDetail={orderDetail} /> } </>
+    )
+  }
+};
 
 export default Order;
